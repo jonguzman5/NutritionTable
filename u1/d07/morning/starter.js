fd34bb78fc4145bd5e66ccf6346737ac73f55058
jonguzman5 <jonguzman5@gmail.com>
2016-10-25 10:19:42 -0400

    push classwork

--- u1/d07/morning/starter.js
+++ u1/d07/morning/starter.js
@@ -1,9 +1,12 @@
 console.log('U01D07 starter.js is connected!');
 
 // DO WORK IN ANY ORDER YOU'D LIKE BELOW, BUT REMEMBER TO HAVE MEANINGFUL VARIABLE NAMES AND CONSOLE.LOG STATEMENTS
-var fulldeck = ['5H', 'QD',]
+var suits = ["S", "H", "D", "C"];
+var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+var fulldeck = suits + values;
 
 var cards = function(){
 var pickedcard = fulldeck[Math.floor(Math.random() * fulldeck.length)];
 return pickedcard;
-}
+};
+cards();
