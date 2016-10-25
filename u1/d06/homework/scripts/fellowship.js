f8dcfe63b22763144d5408329a742a68ee1cc97b
jonguzman5 <jonguzman5@gmail.com>
2016-10-25 09:22:51 -0400

    Add hw for day 6

--- u1/d06/homework/scripts/fellowship.js
+++ u1/d06/homework/scripts/fellowship.js
@@ -22,4 +22,33 @@ var buddies = [
 var lands = ['The Shire', 'Rivendell', 'Mordor'];
 
 // An example of using query selector
-var body = document.querySelector('body');
+
+//1//
+var makeMiddleEarth = function(){
+body = document.querySelector('body');
+body.innerHTML+="<section id = 'middle-earth'></section>";
+sect = document.querySelector('section');
+for(i = 0; i < lands.length; i++){
+ sect.innerHTML += '<article id = "Shire">'+lands[i]+'</article>';
+}};
+makeMiddleEarth();
+
+//2//
+var makeHobbits = function(){
+var getShire = document.querySelectorAll('#Shire')[0];
+getShire.innerHTML += "<ul>"
+for(i = 0; i<hobbits.length; i++){
+getShire.innerHTML += `<li class=${hobbits[i].toLowerCase().split(' ')[0]}>${hobbits[i]}</li>`;
+getShire.innerHTML += "</ul>";
+}};
+makeHobbits();
+//3//
+var keepItSecretKeepItSafe = function(){
+var div = document.createElement('div');
+div.id = "the-ring";
+div.class = 'magic-imbued-jewelry';
+document.getElementsByClassName('frodo').innerHTML += ('the-ring');
+}
+keepItSecretKeepItSafe();
+//4//
+
