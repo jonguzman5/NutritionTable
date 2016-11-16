7da1e1f7455d5e5f797c086ec801cec0566fa4ab
jonguzman5 <jonguzman5@gmail.com>
2016-11-16 07:41:15 -0500

    4

--- u2/d04/homework/app.js
+++ u2/d04/homework/app.js
@@ -2,3 +2,9 @@ var http = require("http");
 var fs = require("fs");
 var port = 8080;
 
+var getHTML = function(res){
+  res.writeHead(200 , {"Content-type": "text/html"});
+  var html = fs.readFileSync(__dirname + "/public/index.html");
+  res.end(html);
+};
+
